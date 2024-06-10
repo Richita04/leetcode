45b7b4d2f6528d155aa798f015ba72c7fc738f0d
Richita Patnaik <134730952+Richita04@users.noreply.github.com>
2024-06-10 22:42:22 +0530

    Time: 1072 ms (5.63%), Space: 52.7 MB (64.04%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,24 +1,17 @@
 class Solution {
     public int majorityElement(int[] nums) {
 
-        int n= nums.length;
+        int n = nums.length;
 
-        HashMap<Integer,Integer> mpp = new HashMap<>();
-        for(int i=0; i<n; i++){
-
-            //Get the value of current element in the array
-            int value= mpp.getOrDefault(nums[i],0);
-
-            //Increment the count of occurrances
-            mpp.put(nums[i], value+1);
-
-        }
-
-        for(Map.Entry<Integer,Integer> ans: mpp.entrySet()){
-            if (ans.getValue() > (n/2)){
-                return ans.getKey();
-            }
-        }
-        return -1;  
+        for(int i = 0; i < n; i++){
+    		int count = 1;
+    		for(int j = i + 1; j < n; j++){
+    			if(nums[i] == nums[j])
+    				count++;
+    		}
+    		if(count > n / 2)
+    			return nums[i];
+    	}
+    	return -1;
     }
 }
