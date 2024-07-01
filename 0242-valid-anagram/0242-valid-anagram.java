dc752764a06a52e2f3bf0aaa36a78e90fd6979cd
Richita Patnaik <134730952+Richita04@users.noreply.github.com>
2024-07-01 15:41:22 +0530

    Time: 4 ms (70.50%), Space: 44.3 MB (47.04%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -7,13 +7,11 @@ class Solution {
 
         char a1[] = s.toCharArray();
         Arrays.sort(a1);
-        s = new String(a1);
 
         char a2[] = t.toCharArray();
         Arrays.sort(a2);
-        t = new String(a2);
 
-        return s.equals(t);
+        return Arrays.equals(a1,a2);
 
            
     }
